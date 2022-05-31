87dbfbd431a59fe28263b358580490320069e32d
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-31 16:18:54 -0400

    Add add-comment.js to public folder

---
+++ public/javascript/add-comment.js
@@ -0,0 +1,8 @@
+// create a comment
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+
+}
+
+document.querySelector('#comment-btn').addEventListener('click', commentFormHandler);
