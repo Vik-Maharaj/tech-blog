bff4a4a284aa89b6128c4e7727af3f11841e24d7
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-04 10:49:18 -0400

    Update edit-post js

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,7 +1,34 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
+    const title = document.querySelector('input[name="title"]').value.trim();
+    const content = document.querySelector('#description').value.trim();
+    const created_at = new Date();
 
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    console.log(title, content, id);
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            content,
+            created_at
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } 
+    else {
+        alert(response.statusText);
+    }
 }
   
 document.querySelector('#update-post-btn').addEventListener('click', editFormHandler);
