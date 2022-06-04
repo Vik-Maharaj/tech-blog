9529fea502005c0e71ca52635d4f1f0700494d13
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-04 10:45:35 -0400

    Update sign-up js

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -1,7 +1,31 @@
 async function signupFormHandler(event) {
     event.preventDefault();
 
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
 
+    if (username && email && password) {
+
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        
+        console.log(response);
     
+        if (response.ok) {
+            console.log('success');
+            document.location.replace('/');
+        } 
+        else {
+            alert(response.statusText);
+        }
+    }
 };
 document.querySelector('#sign-up-form').addEventListener('submit', signupFormHandler);
