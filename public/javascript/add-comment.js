5e21da10bc7b321bb259fba9468e9dd08a692b88
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-06 09:16:34 -0400

    Update add-comment js

--- public/javascript/add-comment.js
+++ public/javascript/add-comment.js
@@ -1,8 +1,32 @@
-// create a comment
 async function commentFormHandler(event) {
     event.preventDefault();
 
+    const comment_text = document.querySelector('#comment-input').value.trim();
 
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    console.log(comment_text);
+
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
 }
 
 document.querySelector('#comment-btn').addEventListener('click', commentFormHandler);
