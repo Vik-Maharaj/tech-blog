587cf52dddb2e80adc878b1e4e574b4936ce4896
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-17 12:03:28 -0400

    Add comment-seeds info

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,6 +1,20 @@
 const { Comment } = require('../models');
 
 
+const commentdata = [
+{
+    comment_text:'Here we go!',
+    user_id:'2',
+    post_id:'1',
+},
+{
+    comment_text:'This is absolutely fascinating.',
+    user_id:'1',
+    post_id:'2',
+}
+
+
+];
 const seedComments = () => Comment.bulkCreate(commentdata);
 
 module.exports = seedComments;
