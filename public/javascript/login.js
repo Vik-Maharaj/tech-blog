96b1d08af1694b39b6be4318f19de3cd5d7d7ec2
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-17 11:06:43 -0400

    Update login.js file

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -4,8 +4,8 @@ async function loginFormHandler(event) {
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
 
+    // if email and password are not empty, this block will retrieve the response and send info to the client
     if (username && password) {
-
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
@@ -15,6 +15,7 @@ async function loginFormHandler(event) {
             headers: { 'Content-Type': 'application/json' }
         });
 
+        // if the response is okay, it is sent to the client and redirected to the dashboard page
         if (response.ok) {
             document.location.replace('/dashboard');
         }
