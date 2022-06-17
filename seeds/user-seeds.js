bfb264edf8ae14b5bcb16e978eaffadc126aed7c
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-17 11:52:27 -0400

    Add user-seeds info

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,4 +1,19 @@
 const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
 
+const userdata = [
+  {
+    username: 'markj',
+    password:'password123',
+    email:'markj@testmail.com',
+  },
+  {
+    username:'tonyt',
+    password:'password123',
+    email:'tonyt@testmail.com'
+  }
+];
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
 module.exports = seedUsers;
