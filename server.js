dda312d56f88d9720e85f4a01473ad74f40b980b
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-17 15:34:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,33 +4,39 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const exphbs = require('express-handlebars');
-
 const helpers = require('./utils/helpers');
-
 const hbs = exphbs.create({ helpers });
 
-
-
-
-
+// this creates a session for each user
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'chihuahuas',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
 app.use(session(sess));
 
+// set up for handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
+// express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// express middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// enables use of routes
 app.use(routes);
 
-
+// enables connection to database and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening on Port'));
+    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}/`));
 });
